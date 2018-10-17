71ad19c090e0fa37118aa94f1fd1699df3e7fb71
samzhuwj <samzhuwj@gmail.com>
2018-10-17 16:24:10 +0800

    break, unordered_map::find

---
+++ unordered_map_find.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <unordered_map>
+
+int main()
+{
+  std::unordered_map<int,char> example = {{1,'a'},{2,'b'}};
+  auto search = example.find(2);
+  
+  if (search != example.end())
+    std::cout << "Found " << search->first << " " << search->second << '\n';
+  else
+    std::cout << "Not found\n";
+}
+
+// output:
+// Found 2 b
